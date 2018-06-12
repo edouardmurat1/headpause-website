dd692380d6484fd3c5702e9b13e472bfdb87f298
Edouard Murat <edouardmurat1@gmail.com>
2018-06-11 23:53:10 -0400

    Add forgotten files from previous commit

---
+++ ninja-fight/src/app/fight/enemyai.ts
@@ -0,0 +1,73 @@
+import { Ninja, Action } from '../ninja.service'
+
+export class EnemyAI {
+
+  playerNinja: Ninja;
+  opponentNinja: Ninja;
+
+  constructor(ai: Ninja, player: Ninja) {
+    this.playerNinja = player;
+    this.opponentNinja = ai;
+  }
+
+  nextMove(): Action {
+    let finalAction: Action;
+    let actions = this.opponentNinja.getActions();
+
+    //actions with enough chakra
+    let chakrableActions = this.filterActionsWithEnoughChakra(actions, this.opponentNinja);
+    if(chakrableActions.length > 0) {
+
+      //actions that could kill
+      let killableActions = this.filterActionsThatCouldKill(chakrableActions, this.playerNinja);
+      if(killableActions.length > 0) {
+
+        //actions that won't use up all chakra
+        let nonUseUpActions = this.filterActionsThatWontUseAllChakra(killableActions, this.opponentNinja);
+        if(nonUseUpActions.length > 0) {
+          finalAction = this.randomAction(nonUseUpActions);
+        } else {
+          finalAction = this.randomAction(killableActions);
+        }
+      } else {
+        finalAction = this.randomAction(chakrableActions);
+      }
+    }
+
+    return finalAction;
+  }
+
+  filterActionsWithEnoughChakra(actions: Action[], ninja: Ninja): Action[] {
+    let newActions: Action[] = [];
+    actions.forEach((action, index) => {
+      if (action.chakra <= ninja.chakra.now ) {
+        newActions.push(action);
+      }
+    });
+    return newActions;
+  }
+
+  filterActionsThatCouldKill(actions: Action[], ninja: Ninja): Action[] {
+    let newActions: Action[] = [];
+    actions.forEach((action, index) => {
+      if (action.damage >= ninja.health.now ) {
+        newActions.push(action);
+      }
+    });
+    return newActions;
+  }
+
+  filterActionsThatWontUseAllChakra(actions: Action[], ninja: Ninja): Action[] {
+    let newActions: Action[] = [];
+    actions.forEach((action, index) => {
+      if (action.chakra < ninja.chakra.now ) {
+        newActions.push(action);
+      }
+    });
+    return newActions;
+  }
+
+  randomAction(actions: Action[]): Action {
+    return actions[Math.floor(Math.random() * actions.length)];;
+  }
+}
