1cbdb63c48706e23dc0d732dfff9da41dc61bfa9
Edouard Murat <edouardmurat1@gmail.com>
2018-08-08 14:45:46 -0400

    Make english version

--- js/all.js
+++ js/all.js
@@ -14,7 +14,7 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top - 57)
+          scrollTop: (target.offset().top - 50)
         }, 1000, "easeInOutExpo");
         return false;
       }
